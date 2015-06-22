6b7997e1da746545f34009fc12241f53252a1fe5
Dietrich <dietricherwane@yahoo.fr>
2015-06-22 11:29:11 +0000

    changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -131,7 +131,7 @@ class MessagesController < ApplicationController
     if msisdn.match(/\./)
       msisdn = "22" + msisdn[0..8]
     end
-    request = Typhoeus::Request.new("http://smsplus3.routesms.com:8080/bulksms/bulksms?username=ngser1&password=abcd1234&type=0&dlr=1&destination=#{msisdn}&source=#{session[:service]}&message=#{URI.escape(@message)}", followlocation: true, method: :get)
+    request = Typhoeus::Request.new("http://smsplus3.routesms.com:8080/bulksms/bulksms?username=ngser1&password=abcd1234&type=0&dlr=1&destination=#{msisdn}&source=#{@service}&message=#{URI.escape(@message)}", followlocation: true, method: :get)
 
     request.on_complete do |response|
       if response.success?
