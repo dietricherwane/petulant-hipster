8dd4793a27bd23b407425074cfd7ff80dda89e23
root <root@de2513.ispfr.net>
2017-08-30 12:44:21 +0200

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830103938) do
+ActiveRecord::Schema.define(version: 20170830100328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
