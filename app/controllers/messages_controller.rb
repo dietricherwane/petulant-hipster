9955abde06971c0bf4b0ed6b0ffe60132a18cc4b
Dietrich <dietricherwane@yahoo.fr>
2017-08-30 10:59:58 +0000

    changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -164,7 +164,7 @@ class MessagesController < ApplicationController
         }
       }
     ]
-    request = Typhoeus::Request.new(sms_provider_url, body: body, followlocation: true, method: :get, headers: { Authorization: "Bearer #{sms_provider_token}", Content-type: "application/json" })
+    request = Typhoeus::Request.new(sms_provider_url, body: body, followlocation: true, method: :post, headers: { Authorization: "Bearer #{sms_provider_token}", Accept: "application/json" })
 
     request.on_complete do |response|
       if response.success?
