4137acb4cf3ca5a6e67f123ba0fa161c118784fd
dietrich <dietricherwane@live.fr>
2019-01-26 18:17:22 +0000

    bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
 	end
 
 	def after_sign_in_path_for(resource_or_scope)
-	  new_user_registration_path
+	  message_path #new_user_registration_path
 	end
 end
